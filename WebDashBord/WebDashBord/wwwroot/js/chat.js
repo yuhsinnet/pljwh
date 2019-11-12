f5e235fd4fdfe0662f73c8167362d16ce6df9fe0
x78701542000 <x78701542000@livemail.tw>
2019-11-12 21:22:01 +0800

    移除不必要的code

--- WebDashBord/WebDashBord/wwwroot/js/chat.js
+++ WebDashBord/WebDashBord/wwwroot/js/chat.js
@@ -1,15 +1,10 @@
 ﻿"use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("https://whw.cjee.tw/ChatHub").build();
-//var connection = new signalR.HubConnectionBuilder().withUrl("/ChatHub").build();
-//Disable send button until connection is established
-//document.getElementById("sendButton").disabled = true;
+
 
 connection.on("JsonGet", (json) => {
-    //const encodedMsg = user + " says " + message;
-    //const li = document.createElement("li");
-    //li.textContent = encodedMsg;
-    //document.getElementById("jjj").value = "data" + json;
+
     document.getElementById("test").innerHTML = json;
 });
 
@@ -22,15 +17,6 @@ connection.start().then(function () {
 
 
 });
-
-//document.getElementById("sendButton").addEventListener("click", function (event) {
-//    var user = document.getElementById("userInput").value;
-//    var message = document.getElementById("messageInput").value;
-//    connection.invoke("SendMessage", user, message).catch(function (err) {
-//        return console.error(err.toString());
-//    });
-//    event.preventDefault();
-//});
 
 function testsub()
 {
