cb5c136f49a3508c693ce032a330d45defc23140
x78701542000 <x78701542000@livemail.tw>
2019-11-21 15:42:28 +0800

    移除不必要程式碼 變更一些介面

--- WebDashBord/WebDashBord/wwwroot/js/chat.js
+++ WebDashBord/WebDashBord/wwwroot/js/chat.js
@@ -2,18 +2,10 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("https://whw.cjee.tw/ChatHub").build();
 
-//var jsonArray;
 
 connection.on("JsonGet", (json) => {
-    //jsonArray = json;
-    
-    //document.getElementById("test").innerHTML = jsonArray;
-    //appendTable(jsonArray);
-
 
     var obj = JSON.parse(json);
-
-    //document.getElementById("test").innerHTML = obj[0].Value;
 
 
 
@@ -40,101 +32,22 @@ for (var i = 0; i < obj.length; i++  ) {
          "</tr>";
 }
 txt += "</table>"    
-document.getElementById("test").innerHTML = txt;
+    document.getElementById("SensorTable").innerHTML = txt;
 
 
 
 
 });
-
-
-
-
-
-
-var headArray = [];
-    function parseHead(oneRow) {
-        debugger  
-        for (var i in oneRow) {
-            
-            headArray[headArray.length] = i;
-            console.log(i);
-
-            
-
-
-        }
-    }
-    
-
-    function appendTable() {
-
-        parseHead(jsonArray[0]);
-        var div = document.getElementById("div1");
-        var table = document.createElement("table");
-        var thead = document.createElement("tr");
-
-
-        for (var count = 0; count < headArray.length; count++) {
-
-            var td = document.createElement("th");
-            td.innerHTML = headArray[count];
-            thead.appendChild(td);
-        }
-        table.appendChild(thead);
-
-
-
-
-        for (var tableRowNo = 0; tableRowNo < jsonArray.length; tableRowNo++) {
-
-            var tr = document.createElement("tr");
-            for (var headCount = 0; headCount < headArray.length; headCount++) {
-                var cell = document.createElement("td");
-                cell.innerHTML = jsonArray[tableRowNo][headArray[headCount]];
-                tr.appendChild(cell);
-            }
-            table.appendChild(tr);
-        }
-        div.appendChild(table);
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 connection.start().then(function () {
-    document.getElementById("jjj").value = "startconnectV#";
+    document.getElementById("ConnectStat").innerHTML = "連線成功";
     
     //document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
-    document.getElementById("demo").innerHTML = err;
+    document.getElementById("ConnectStat").innerHTML = err;
 
 
 });
 
-
-
-
-
-
-
-
-
-
-function testsub()
-{
-    document.getElementById("jjj").value = "WWWW";
-}
 
